adebfcab8ee379544df62e21ce696f765d25bd2b
nsurya-0698 <nsuryateja1997@gmail.com>
2021-12-27 11:17:18 -0500

    Some updations

--- Number gussing game /Number_Gussing_Game.py
+++ Number gussing game /Number_Gussing_Game.py
@@ -3,8 +3,10 @@ import time
 print("Welcom to the game")
 
 ready = input("Ready to start Game? ")
+top_range = str(int(input("Give me a number ")))
+
 number_of_attemptes = 0
-if ready.lower() == "yes":
+if ready.lower() == "yes" and top_range.isdigit():
     print("Let's Go")
 else:
     print("See you next time ")
@@ -13,12 +15,13 @@ print("Loading a number.......")
 time.sleep(3)
 
 while True: 
-        number = str(input("Guss my number ranging from 0 to 10! "))
+        number = str(input("Guss my number ranging from 0 to the number you entered! "))
         number_of_attemptes += 1
         
         if number.isdigit() and int(number) >= 0 and int(number) <= 10:
             number = int(number)
-            gussed_int = random.randint(0, number)
+            gussed_int = random.randint(0, int(top_range))
+
             if number > gussed_int:
                 print("You are above the numebr")
             elif number < gussed_int:
@@ -28,4 +31,4 @@ while True:
                 print("Number of attempts {}".format(number_of_attemptes))
                 break
         else:
-            print("Enter a Valid integer from 0 to 10")
+            print("Enter a Valid integer from 0 to number you entered")
